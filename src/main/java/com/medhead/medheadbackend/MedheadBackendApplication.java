0138197717e06227d56419270f68ca59e3ed7fbc
Louis ZEPHIR <louis.zephir@y-nissy.com>
2025-11-06 23:37:20 +0100

    Premier commit - Structure de base projet Spring

---
+++ src/main/java/com/medhead/medheadbackend/MedheadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.medheadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedheadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedheadBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medhead/medheadbackend/MedheadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.medheadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedheadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
